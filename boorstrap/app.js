2264b6985e21842e1f130501fe538c6a35330869
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-30 11:14:23 +0530

    done

---
+++ boorstrap/app.js
@@ -0,0 +1,16 @@
+// AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  // autoplay: {
+  //   delay: 1800,
+  //   disableOnInteraction: false,
+  // },
+});
